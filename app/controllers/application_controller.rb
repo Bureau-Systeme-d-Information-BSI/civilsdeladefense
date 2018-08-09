7d978e0f083c033c0ad27417c49ac420c69f5fbd
Yann Klis <yann.klis@gmail.com>
2018-08-09 12:06:20 +0200

    added basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
 
+  before_action :basic_auth, if: -> { ENV['BASIC_AUTH'].present? }
+
   layout :layout_by_resource
 
   private
+
+    def basic_auth
+      authenticate_or_request_with_http_basic do |u1, p1|
+        u2, p2 = ENV['BASIC_AUTH'].split(":")
+        u1 == u2 && p1 == p2
+      end
+    end
 
     def layout_by_resource
       if devise_controller? && resource_name == :admin

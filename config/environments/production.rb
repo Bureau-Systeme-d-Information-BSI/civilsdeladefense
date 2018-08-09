c5e54a988f4d625e0ca074fdd2496e9c424c92c7
Yann Klis <yann.klis@gmail.com>
2018-08-09 12:27:26 +0200

    read MAIL_URL in production to send email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: ENV['DEFAULT_HOST']}
 
+  smtp_uri = URI(ENV['MAIL_URL'])
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: smtp_uri.host,
+    port: smtp_uri.port,
+    authentication: :login,
+    user_name: URI.unescape(smtp_uri.user),
+    password: smtp_uri.password,
+    enable_starttls_auto: true,
+  }
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true

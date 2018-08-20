625aa2461732591a3a6def781d23343bc3dc762c
Yann Klis <yann.klis@gmail.com>
2018-08-20 16:40:16 +0200

    job application badge colors

--- app/helpers/job_applications_helper.rb
+++ app/helpers/job_applications_helper.rb
@@ -1,2 +1,20 @@
 module JobApplicationsHelper
+  def badge_color_for_state(state)
+    case state
+    when :initial
+      'purple'
+    when :rejected, :phone_meeting_rejected, :after_meeting_rejected
+      'light-gray'
+    when :phone_meeting_accepted, :accepted
+      'success'
+    when :contract_drafting, :contract_feedback_waiting, :contract_received, :affected
+      'primary'
+    when :phone_meeting, :to_be_met
+      'info'
+    end
+  end
+
+  def badge_class(state)
+    "badge-#{ badge_color_for_state(state) }"
+  end
 end

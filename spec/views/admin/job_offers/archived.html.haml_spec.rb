a66f7bd910c3481d45635721a495486819170d29
Yann Klis <yann.klis@gmail.com>
2018-09-03 08:52:21 +0200

    added job_offers archived view test

---
+++ spec/views/admin/job_offers/archived.html.haml_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "admin/job_offers/archived", type: :view do
+  before(:each) do
+    ary1 = assign(:job_offers_active, [
+      create(:job_offer, state: :published),
+      create(:job_offer, state: :published),
+    ])
+    ary2 = assign(:job_offers_archived, [
+      create(:job_offer, state: :archived),
+      create(:job_offer, state: :archived),
+      create(:job_offer, state: :archived),
+    ])
+    assign(:job_offers, ary2)
+  end
+
+  it "renders a list of job_offers" do
+    render_template('/admin/job_offers/index')
+    # assert_select "tr>td", :text => "Title".to_s, :count => 2
+    # assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    # assert_select "tr>td", :text => nil.to_s, :count => 2
+    # assert_select "tr>td", :text => nil.to_s, :count => 2
+    # assert_select "tr>td", :text => "Location".to_s, :count => 2
+    # assert_select "tr>td", :text => nil.to_s, :count => 2
+    # assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    # assert_select "tr>td", :text => "MyText".to_s, :count => 2
+    # assert_select "tr>td", :text => nil.to_s, :count => 2
+    # assert_select "tr>td", :text => false.to_s, :count => 2
+    # assert_select "tr>td", :text => nil.to_s, :count => 2
+    # assert_select "tr>td", :text => nil.to_s, :count => 2
+    # assert_select "tr>td", :text => nil.to_s, :count => 2
+    # assert_select "tr>td", :text => false.to_s, :count => 2
+    # assert_select "tr>td", :text => "Estimate Monthly Salary Net".to_s, :count => 2
+    # assert_select "tr>td", :text => "Estimate Monthly Salary Gross".to_s, :count => 2
+  end
+end

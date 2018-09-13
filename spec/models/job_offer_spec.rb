b3ae0ea24a7593762a411e846d4e05fa2ed14193
N1colas <agostinicolas@gmail.com>
2018-09-13 15:46:31 +0200

    implémentation des tests pour la regle de gestion entre les type de contrat et la durée de contrat #20

--- spec/models/job_offer_spec.rb
+++ spec/models/job_offer_spec.rb
@@ -3,3 +3,29 @@ require 'rails_helper'
 RSpec.describe JobOffer, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 end
+
+RSpec.describe JobOffer do
+	it 'should be invalid if duration <> nil when type = CDI' do
+		leType = build(:contract_type, name: 'CDI')
+		jb = build(:job_offer, contract_type: leType, duration_contract: "2 mois")
+		jb.valid?
+		expect(jb.duration_contract).to be_nil
+	end
+
+	it 'should be invalid if duration <> nil when type = Interim' do
+		leType = build(:contract_type, name: 'Interim')
+		jb = build(:job_offer, contract_type: leType, duration_contract: "2 mois")
+		jb.valid?
+		expect(jb.duration_contract).to be_nil
+	end
+
+	it 'should be invalid if duration is edit when type = CDD' do
+		leType = build(:contract_type, name: 'CDD')
+		duration = "2 mois"
+		jb = build(:job_offer, contract_type: leType, duration_contract: duration)
+		jb.valid?
+		expect(jb.duration_contract).to match(duration)
+	end
+
+	
+end

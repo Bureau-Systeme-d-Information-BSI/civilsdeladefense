eec88ce0b71880097a6dd2413a8755305c9891c7
Yann Klis <yann.klis@gmail.com>
2018-11-15 17:56:03 +0100

    secured file access fix #140

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -35,24 +35,28 @@ module UsersHelper
   end
 
 
-  def hint_text_for_file(obj, file_name, is_validated_value)
+  def hint_text_for_file(job_application, obj, file_name, is_validated_value)
     file = obj.send(file_name.to_sym)
     txt = []
     if is_validated_value == 1
-      link = file.url
+      link = link_for_file(job_application, file_name)
       txt << "Vous pouvez #{ link_to "consulter", link, target: '_blank', class: 'text-dark-gray' } ce fichier.".html_safe
     elsif is_validated_value == 2
-      link = file.url
+      link = link_for_file(job_application, file_name)
       txt << "Votre #{ link_to "fichier", link, target: '_blank', class: 'text-dark-gray' } n'est pas valide, veuillez en téléverser un nouveau.".html_safe
       txt << "Seuls les fichiers PDF de taille inférieure à 2Mo sont acceptés."
     else
       if file.exists?
-        link = file.url
+        link = link_for_file(job_application, file_name)
         txt << "Vous avez déjà téléversé ce #{ link_to "fichier", link, target: '_blank', class: 'text-dark-gray' }, il est en attente de validation.".html_safe
         txt << "Pour téléverser une nouvelle version, vous pouvez utiliser la zone ci-dessus.".html_safe
       end
       txt << "Seuls les fichiers PDF de taille inférieure à 2Mo sont acceptés."
     end
     txt.join('<br/>').html_safe
+  end
+
+  def link_for_file job_application, file_name
+    send("account_job_application_#{file_name}_url", job_application, format: :pdf)
   end
 end

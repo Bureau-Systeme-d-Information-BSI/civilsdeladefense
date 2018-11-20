0a4e32ca78d2eafc7143f6c621d17a91e949f135
Yann Klis <yann.klis@gmail.com>
2018-11-20 14:09:17 +0100

    corrected routing test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,21 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+module DeviseRoutingHelpers
+  # workaround for this issue https://github.com/plataformatec/devise/issues/1670
+  def mock_warden_for_route_tests!
+    warden = double
+    allow_any_instance_of(ActionDispatch::Request).to receive(:env).
+      and_wrap_original { |orig, *args|
+      env = orig.call(*args)
+      env['warden'] = warden
+      env
+    }
+    allow(warden).to receive(:authenticate!).and_return(true)
+  end
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -67,4 +82,11 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :view
   config.extend ControllerMacros, type: :view
   config.include Devise::Test::IntegrationHelpers, type: :request
+
+
+  config.include DeviseRoutingHelpers, type: :routing
+
+  config.before(:each, type: :routing) do
+    mock_warden_for_route_tests!
+  end
 end
--- spec/routing/account/emails_routing_spec.rb
+++ spec/routing/account/emails_routing_spec.rb
@@ -3,11 +3,11 @@ require "rails_helper"
 RSpec.describe Account::EmailsController, type: :routing do
   describe "routing" do
     it "routes to #index" do
-      expect(:get => "/mes-candidatures/1/emails").to route_to("account/emails#index", job_application_id: '1')
+      expect(get: "/mes-candidatures/1/emails").to route_to("account/emails#index", job_application_id: '1')
     end
 
     it "routes to #create" do
-      expect(:post => "/mes-candidatures/1/emails").to route_to("account/emails#create", job_application_id: '1')
+      expect(post: "/mes-candidatures/1/emails").to route_to("account/emails#create", job_application_id: '1')
     end
   end
 end

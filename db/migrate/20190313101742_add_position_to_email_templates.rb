6b668860fb45ce7baa89b4d57609e2737c7407e4
Yann Klis <yann.klis@gmail.com>
2019-03-13 11:23:08 +0100

    allow email templates ordering fix #248

---
+++ db/migrate/20190313101742_add_position_to_email_templates.rb
@@ -0,0 +1,9 @@
+class AddPositionToEmailTemplates < ActiveRecord::Migration[5.2]
+  def change
+    add_column :email_templates, :position, :integer
+    EmailTemplate.order(:updated_at).each.with_index(1) do |item, index|
+      item.update_column :position, index
+    end
+    add_index :email_templates, :position
+  end
+end

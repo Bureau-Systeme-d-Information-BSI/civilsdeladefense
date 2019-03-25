e42d1464a8250251b68791314938c84f6fabdf0b
Yann Klis <yann.klis@gmail.com>
2019-03-25 09:29:59 +0100

    send daily summary

--- clock.rb
+++ clock.rb
@@ -14,4 +14,10 @@ module Clockwork
   every 5.minutes, "inbound_message_fetch_and_process" do
     InboundMessage.fetch_and_process
   end
+
+  if ENV['SEND_DAILY_SUMMARY'].present?
+    every 1.day, "inbound_message_fetch_and_process", at: '08:00' do
+      DailySummary.new(day: 1.day.ago).prepare_and_send
+    end
+  end
 end

4d2f90a1e46ab5e7a316ece054b646a6fa981082
Yann Klis <yann.klis@gmail.com>
2019-04-15 11:48:09 +0200

    allow employer and BRH to see documents attached to an application fix #268

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -11,6 +11,7 @@ class Ability
         can :create, JobOffer
         can :manage, JobOffer, job_offer_actors: {administrator_id: administrator.id}
         can :manage, JobApplication, job_offer: {job_offer_actors: {administrator_id: administrator.id}}
+        can :manage, JobApplicationFile
         can :manage, Message
         can :manage, Email
       else
@@ -18,6 +19,7 @@ class Ability
         can :read, JobApplication, job_offer: {job_offer_actors: {administrator_id: administrator.id}}
         role_brh_enum = JobOfferActor.roles[:brh]
         can :manage, JobApplication, job_offer: {job_offer_actors: {administrator_id: administrator.id, role: role_brh_enum}}
+        can :manage, JobApplicationFile
         can :read, Message
         can :read, Email
       end

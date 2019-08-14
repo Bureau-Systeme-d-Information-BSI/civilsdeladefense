8e4975826227719b39b2d30f37adca70a5362076
Yann Klis <yann.klis@gmail.com>
2019-08-14 12:03:12 +0200

    new statistic line

--- app/controllers/concerns/job_offer_statistics_actions.rb
+++ app/controllers/concerns/job_offer_statistics_actions.rb
@@ -7,6 +7,7 @@ module JobOfferStatisticsActions
   # GET /admin/job_offers/1/stats
   # GET /admin/job_offers/1/stats.json
   def stats
+    @job_applications_count = @job_offer.job_applications.count
     @per_day = @job_offer.job_applications.unscope(:order)
                          .group_by_day(:created_at, range: date_start..date_end).count
 
@@ -15,9 +16,15 @@ module JobOfferStatisticsActions
     root_rel = @job_offer.job_applications
                          .unscope(:order)
                          .where(job_applications: { created_at: date_start..date_end })
+    root_rel_profile = root_rel.joins(:personal_profile)
 
-    @per_gender = root_rel.joins(:personal_profile).group(:gender).count
-    @per_nationality = root_rel.joins(:personal_profile).group(:nationality).count
+    @per_gender = root_rel_profile.group(:gender).count
+    @per_nationality = root_rel_profile.group(:nationality).count
+    @experiences_fit_job_offer_count = root_rel.where(experiences_fit_job_offer: true).count
+    opts = { personal_profiles: { has_corporate_experience: true }}
+    @has_corporate_experience_count = root_rel_profile.where(opts).count
+    opts = { personal_profiles: { is_currently_employed: true }}
+    @is_currently_employed_count = root_rel_profile.where(opts).count
     @per_state = root_rel.group(:state).count
     @per_rejection_reason = root_rel.group(:rejection_reason_id).count
     @rejection_reasons = RejectionReason.all

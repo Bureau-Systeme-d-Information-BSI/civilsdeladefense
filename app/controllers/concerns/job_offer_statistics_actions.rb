6b263bec7204f72ed79814fd91a49ba6e729cfc9
Yann Klis <yann.klis@gmail.com>
2019-08-19 17:25:20 +0200

    only stats when meaningful fix #321

--- app/controllers/concerns/job_offer_statistics_actions.rb
+++ app/controllers/concerns/job_offer_statistics_actions.rb
@@ -19,11 +19,9 @@ module JobOfferStatisticsActions
 
     @per_gender = root_rel_profile.group(:gender).count
     @per_nationality = root_rel_profile.group(:nationality).count
-    @experiences_fit_job_offer_count = root_rel.where(experiences_fit_job_offer: true).count
-    opts = { personal_profiles: { has_corporate_experience: true } }
-    @has_corporate_experience_count = root_rel_profile.where(opts).count
-    opts = { personal_profiles: { is_currently_employed: true } }
-    @is_currently_employed_count = root_rel_profile.where(opts).count
+    @per_experiences_fit_job_offer = root_rel.group(:experiences_fit_job_offer).count
+    @per_has_corporate_experience = root_rel_profile.group(:has_corporate_experience).count
+    @per_is_currently_employed = root_rel_profile.group(:is_currently_employed).count
     @per_state = root_rel.group(:state).count
     @per_rejection_reason = root_rel.group(:rejection_reason_id).count
     @rejection_reasons = RejectionReason.all

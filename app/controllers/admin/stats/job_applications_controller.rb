cffee1d7e2e33544425ac1ee0d4c92ac73020768
Yann Klis <yann.klis@gmail.com>
2019-08-21 17:10:45 +0200

    silenced rubocop complains

--- app/controllers/admin/stats/job_applications_controller.rb
+++ app/controllers/admin/stats/job_applications_controller.rb
@@ -1,62 +1,37 @@
 # frozen_string_literal: true
 
 class Admin::Stats::JobApplicationsController < Admin::Stats::BaseController
+  before_action :filter_by_full_text, only: %i[index], if: -> { params[:s].present? }
+  before_action :fetch_base_ressources, only: %i[index]
+
   # GET /admin/stats/candidatures
   # GET /admin/stats/candidatures.json
   def index
-    range = date_start..date_end if date_start.present? && date_end.present?
-    @employers = Employer.all
-    if params[:s].present?
-      @job_applications = @job_applications.search_full_text(params[:s])
-                                           .unscope(:order)
-    end
-    @job_applications = @job_applications.where(created_at: range)
+    @job_applications = @job_applications.where(created_at: date_range)
     @q = @job_applications.ransack(params[:q])
     @job_applications_filtered = @q.result(distinct: true)
                                    .page(params[:page])
                                    .per_page(20)
     @job_applications_count = @job_applications_filtered.count
-    @per_day = @q.result(distinct: true)
-                 .unscope(:order)
-                 .group_by_day(:created_at, range: range)
-                 .count
-    @per_gender = @q.result(distinct: true)
-                    .unscope(:order)
-                    .joins(:personal_profile)
-                    .group(:gender)
-                    .count
-    @per_nationality = @q.result(distinct: true)
-                         .unscope(:order)
-                         .joins(:personal_profile)
-                         .group(:nationality)
-                         .count
 
-    @per_experiences_fit_job_offer = @q.result(distinct: true)
-                                       .unscope(:order)
-                                       .group(:experiences_fit_job_offer)
-                                       .count
-    @per_has_corporate_experience = @q.result(distinct: true)
-                                      .unscope(:order)
-                                      .joins(:personal_profile)
-                                      .group(:has_corporate_experience)
-                                      .count
-    @per_is_currently_employed = @q.result(distinct: true)
-                                   .unscope(:order)
-                                   .joins(:personal_profile)
-                                   .group(:is_currently_employed)
-                                   .count
-    @per_rejection_reason = @q.result(distinct: true)
-                              .unscope(:order)
-                              .group(:rejection_reason_id)
-                              .count
-    @per_state = @q.result(distinct: true)
-                   .unscope(:order)
-                   .group(:state)
-                   .count
-    @rejection_reasons = RejectionReason.all
+    root_rel = @q.result(distinct: true).unscope(:order)
+    root_rel_profile = root_rel.joins(:personal_profile)
+
+    @per_day = root_rel.group_by_day(:created_at, range: date_range).count
+    @per_gender = root_rel_profile.group(:gender).count
+    @per_nationality = root_rel_profile.group(:nationality).count
+    @per_experiences_fit_job_offer = root_rel.group(:experiences_fit_job_offer).count
+    @per_has_corporate_experience = root_rel_profile.group(:has_corporate_experience).count
+    @per_is_currently_employed = root_rel_profile.group(:is_currently_employed).count
+    @per_rejection_reason = root_rel.group(:rejection_reason_id).count
+    @per_state = root_rel.group(:state).count
   end
 
   protected
+
+  def filter_by_full_text
+    @job_applications = @job_applications.search_full_text(params[:s]).unscope(:order)
+  end
 
   def date_start
     @date_start ||= begin
@@ -70,5 +45,14 @@ class Admin::Stats::JobApplicationsController < Admin::Stats::BaseController
       res = Date.parse(params[:end]) if params[:end].present?
       res || Time.current
     end
+  end
+
+  def date_range
+    date_start..date_end if date_start.present? && date_end.present?
+  end
+
+  def fetch_base_ressources
+    @employers = Employer.all
+    @rejection_reasons = RejectionReason.all
   end
 end

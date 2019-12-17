bee279f3ca8fb6545ff971071fcd78fa636c4d2a
Yann Klis <yann.klis@gmail.com>
2019-12-17 17:26:35 +0100

    correct ordering of rejection reasons and categories fix #384 redisplay name/title fix #383

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_144016) do
+ActiveRecord::Schema.define(version: 2019_12_17_161709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
@@ -118,7 +118,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_144016) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "position"
     t.uuid "parent_id"
     t.integer "lft"
     t.integer "rgt"

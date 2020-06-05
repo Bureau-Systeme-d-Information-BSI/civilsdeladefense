8b53152e8b69f13b7f6632827eaedc88a5c731d3
Yann Klis <yann.klis@gmail.com>
2020-06-05 15:07:26 +0200

    replaced tinymce by trix editor

--- app/javascript/packs/admin-style.js
+++ app/javascript/packs/admin-style.js
@@ -1,6 +1,5 @@
 import 'select2/dist/css/select2.css'
 import 'flatpickr/dist/flatpickr.css'
 import 'lightpick/css/lightpick.css'
-import 'tinymce/skins/ui/oxide/content.css'
-import 'tinymce/skins/ui/oxide/skin.css'
+import 'trix/dist/trix.css'
 require('css/admin.scss')

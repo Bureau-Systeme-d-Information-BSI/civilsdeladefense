89f5ad260e5863a2debb5184fcdba0ba1089035b
Yann Klis <yann.klis@gmail.com>
2020-12-14 09:24:01 +0100

    activate each cron job conditionnaly

--- clock.rb
+++ clock.rb
@@ -14,20 +14,26 @@ module Clockwork
     config[:tz] = 'Europe/Paris'
   end
 
-  every 5.minutes, 'inbound_message_fetch_and_process' do
-    InboundMessage.fetch_and_process
+  cond = ENV['CRON_FETCH_INBOUND'].present?
+  if cond
+    every 5.minutes, 'inbound_message_fetch_and_process' do
+      InboundMessage.fetch_and_process
+    end
   end
 
-  cond = ENV['NBR_DAYS_INACTIVITY_PERIOD_BEFORE_DELETION'].present?
+  cond = ENV['CRON_PURGE_USERS'].present?
+  cond &&= ENV['NBR_DAYS_INACTIVITY_PERIOD_BEFORE_DELETION'].present?
   cond &&= ENV['NBR_DAYS_NOTICE_PERIOD_BEFORE_DELETION'].present?
   if cond
-    every 1.day, 'inbound_message_fetch_and_process', at: '03:00' do
+    every 1.day, 'purge_users', at: '03:00' do
       User.destroy_when_too_old
     end
   end
 
-  if ENV['SEND_DAILY_SUMMARY'].present?
-    every 1.day, 'inbound_message_fetch_and_process', at: '08:00' do
+  cond = ENV['CRON_DAILY_SUMMARY'].present?
+  cond ||= ENV['SEND_DAILY_SUMMARY'].present?
+  if cond
+    every 1.day, 'daily_summary', at: '08:00' do
       DailySummary.new(day: 1.day.ago).prepare_and_send
     end
   end

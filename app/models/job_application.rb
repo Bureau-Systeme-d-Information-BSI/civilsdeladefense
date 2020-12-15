93051aca02a4ecff6f3d2cb21789d8c6493f0197
Yann Klis <yann.klis@gmail.com>
2020-12-15 09:17:40 +0100

    compute file notification fix #735

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -143,25 +143,14 @@ class JobApplication < ApplicationRecord
   end
 
   def compute_files_count
-    # ary = JobOffer::FILES.inject([0, 0]) { |memo,obj|
-    #   if self.send("#{obj}?".to_sym)
-    #     memo[0] += 1
-    #     if self.send("#{obj}_is_validated".to_sym) == 0
-    #       memo[1] += 1
-    #     end
-    #   end
-    #   memo
-    # }
-    # ary = (User::FILES_JUST_AFTER_SUBMISSION + User::FILES_FOR_PAYROLL).inject(ary) { |memo,obj|
-    #   if self.user.send("#{obj}?".to_sym)
-    #     memo[0] += 1
-    #     if self.user.send("#{obj}_is_validated".to_sym) == 0
-    #       memo[1] += 1
-    #     end
-    #   end
-    #   memo
-    # }
-    ary = [0, 0]
+    ary_start = [0, 0]
+    ary = job_application_files.inject(ary_start) do |memo, job_application_file|
+      memo[0] += 1
+      if job_application_file.is_validated == 0
+        memo[1] += 1
+      end
+      memo
+    end
     self.files_count, self.files_unread_count = ary
   end
 

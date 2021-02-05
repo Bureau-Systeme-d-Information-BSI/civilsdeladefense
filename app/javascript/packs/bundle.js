047ff2fca6de5ed335ae1418794edef437a7e6b2
Yann Klis <yann.klis@gmail.com>
2021-02-05 11:32:14 +0100

    reset form with stimulus

--- app/javascript/packs/bundle.js
+++ app/javascript/packs/bundle.js
@@ -14,5 +14,12 @@ function importAll(r) {
 importAll(require.context('images/', true, /\.(ico|png|jpe?g|svg|gif)$/))
 importAll(require.context('icons/', true, /\.svg$/))
 
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+
 import { Turbo } from "@hotwired/turbo-rails"
 window.Turbo = Turbo

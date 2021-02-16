44164398f75f7ad252e4856f57737e12b5b5cff3
Jean Bertrand <jean@scalingo.com>
2021-02-16 11:23:16 +0100

    feat: notify Rollbar of deployment

--- bin/postdeploy.sh
+++ bin/postdeploy.sh
@@ -1,7 +1,12 @@
+#!/usr/bin/env bash
+set -x
+
 if [ "$DO_NOT_POSTDEPLOY" = "1" ] ; then
   echo "Postdeploy hook disabled"
   exit 0
 else
   exec rails db:migrate
-  curl https://api.rollbar.com/api/1/deploy/ -F access_token=$ROLLBAR_ACCESS_TOKEN -F environment=$ROLLBAR_ENV -F revision=$CONTAINER_VERSION -F local_username=scalingo
+  if [[ -z "${IS_REVIEW_APP}" ]]; then
+    curl https://api.rollbar.com/api/1/deploy/ -F access_token=$ROLLBAR_ACCESS_TOKEN -F environment=$ROLLBAR_ENV -F revision=$CONTAINER_VERSION -F local_username=scalingo
+  fi
 fi

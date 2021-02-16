534697e86f4ab2f06baf63f8fbeb56b36bdf46b4
Jean Bertrand <jean@scalingo.com>
2021-02-16 11:23:16 +0100

    feat: notify Rollbar of deployment

--- bin/postdeploy.sh
+++ bin/postdeploy.sh
@@ -3,4 +3,5 @@ if [ "$DO_NOT_POSTDEPLOY" = "1" ] ; then
   exit 0
 else
   exec rails db:migrate
+  curl https://api.rollbar.com/api/1/deploy/ -F access_token=$ROLLBAR_ACCESS_TOKEN -F environment=$ROLLBAR_ENV -F revision=$CONTAINER_VERSION -F local_username=scalingo
 fi

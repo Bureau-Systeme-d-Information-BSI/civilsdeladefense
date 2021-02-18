8654099f61145e9c6dbd4aa2dc68fa052344b35a
Jean Bertrand <jean@scalingo.com>
2021-02-18 21:17:41 +0100

    feat: add new job offer filters
    fix #762
    fix #763
    fix #764
    fix #765
    fix #225

--- spec/views/job_offers/index.html.haml_spec.rb
+++ spec/views/job_offers/index.html.haml_spec.rb
@@ -6,22 +6,14 @@ require "will_paginate/array"
 RSpec.describe "job_offers/index", type: :view do
   before(:each) do
     categories = create_list(:category, 5)
-
-    owner = create(:owner)
-    category = categories.first
-    employer = create(:employer)
-
-    job_offers = create_list(:job_offer,
-      2,
-      state: :published,
-      owner: owner,
-      category: category,
-      employer: employer)
+    job_offers = create_list(:job_offer, 30)
 
     assign(:categories, categories)
-    assign(:categories_for_select, categories)
     assign(:contract_types, contract_types)
     assign(:job_offers, job_offers.paginate(page: nil))
+    assign(:study_levels, study_levels)
+    assign(:experience_levels, experience_levels)
+    assign(:regions, job_offers.pluck(:region))
   end
 
   it "renders a list of job_offers" do

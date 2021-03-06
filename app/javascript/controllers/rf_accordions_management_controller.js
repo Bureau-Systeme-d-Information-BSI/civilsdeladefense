facdf46c8504587cc165b104b9ba55f96fdf4d93
Yann Klis <yann.klis@gmail.com>
2021-03-06 17:52:16 +0100

    make accordion actually work

--- app/javascript/controllers/rf_accordions_management_controller.js
+++ app/javascript/controllers/rf_accordions_management_controller.js
@@ -1,10 +1,21 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = [ 'section', 'button', 'collapsible' ]
+  static targets = ['section', 'collapsible']
 
-  connect() {
-    // var height = this.panelTarget.offsetHeight + this.tablistTarget.offsetHeight
-    // this.element.style.height = height + 'px'
+  collapse() {
+    var button = event.currentTarget
+    var state = button.getAttribute('aria-expanded')
+    if (state == 'true') {
+      button.setAttribute('aria-expanded', false)
+      this.collapsibleTarget.classList.remove('rf-collapse--expanded')
+    } else {
+      button.setAttribute('aria-expanded', true)
+      this.collapsibleTarget.classList.add('rf-collapse--expanded')
+      this.collapsibleTarget.style.setProperty('--collapser', 'none')
+      const height = this.collapsibleTarget.offsetHeight
+      this.collapsibleTarget.style.setProperty('--collapse', -height + 'px')
+      this.collapsibleTarget.style.setProperty('--collapser', '')
+    }
   }
 }

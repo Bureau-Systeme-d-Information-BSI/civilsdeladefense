d1791818fb701983d6f2023fab06e4c89c55f030
Jean Bertrand <jean@scalingo.com>
2021-03-17 09:57:35 +0100

    fix: allow migration to run

--- db/migrate/20210213195636_rename_brand_prefix_article.rb
+++ db/migrate/20210213195636_rename_brand_prefix_article.rb
@@ -2,6 +2,7 @@
 
 class RenameBrandPrefixArticle < ActiveRecord::Migration[6.1]
   def change
-    rename_column :organizations, :brand_prefix_article, :prefix_article
+    # There is no brand_prefix_article ?!
+    # rename_column :organizations, :brand_prefix_article, :prefix_article
   end
 end

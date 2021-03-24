fb0ad4ad9b4d14d38e00f9846c271ff612b93126
Jean Bertrand <jean@scalingo.com>
2021-03-24 09:28:04 +0100

    refactor: files are always encrypted

--- app/controllers/concerns/send_file_content.rb
+++ app/controllers/concerns/send_file_content.rb
@@ -5,49 +5,13 @@ module SendFileContent
   include ActionController::Live
 
   def send_job_application_file_content
-    if ENV["OSC_AK"].present?
-      proxy_encrypted_job_application_file_content
-    else
-      send_content_directly
-    end
+    send_data(
+      @job_application_file.content.read,
+      filename: "#{action_name}.pdf",
+      type: "application/pdf",
+      disposition: "inline"
+    )
   ensure
     response.stream.close if response.stream.respond_to?(:close)
-  end
-
-  protected
-
-  def send_content_directly
-    send_file(@job_application_file.content.path,
-      disposition: "inline",
-      filename: "#{action_name}.pdf",
-      type: "application/pdf")
-  end
-
-  def proxy_job_application_file_content
-    url = @job_application_file.content.url
-    uri = URI(url)
-    uri = URI("https:#{uri}") if uri.scheme.blank?
-    response.headers["Content-Type"] = "application/pdf"
-    response.headers["Content-Disposition"] = "inline; filename=\"#{action_name}.pdf\""
-    # Download the backup file chunk by chunk and forward each chunk to the client.
-    Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == "https") do |http|
-      req = Net::HTTP::Get.new(uri.request_uri)
-      # req.basic_auth uri.user, uri.password
-      http.request req do |res|
-        # Read fragment of the body from the socket.
-        # The variable `c` contains the fragment's bytes.
-        # I am not really sure about the size of the fragments or how to specify it.
-        res.read_body do |c|
-          response.stream.write c
-        end
-      end
-    end
-  end
-
-  # use Lockbox read method to send file back to the browser
-  def proxy_encrypted_job_application_file_content
-    send_data @job_application_file.content.read, filename: "#{action_name}.pdf",
-                                                  type: "application/pdf",
-                                                  disposition: "inline"
   end
 end

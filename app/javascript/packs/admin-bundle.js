dc5051a6349e64bcc34692d21b0ee9896abc018c
Jean Bertrand <jean@scalingo.com>
2021-03-24 09:27:01 +0100

    feat: Show limit to user

--- app/javascript/packs/admin-bundle.js
+++ app/javascript/packs/admin-bundle.js
@@ -7,6 +7,13 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
 import Turbolinks from 'turbolinks'
 Turbolinks.start()
 

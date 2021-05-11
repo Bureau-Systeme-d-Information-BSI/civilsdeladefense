f266fada0603a9c3187fb9acca6c59e5c351bc56
Jean Bertrand <jean@scalingo.com>
2021-05-11 11:55:02 +0200

    feat: select multiples list

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,11 +19,14 @@ class Admin::UsersController < Admin::InheritedResourcesController
 
   def multi_select
     users = User.where(id: params[:user_ids])
-    if params[:list_id]
-      list = current_administrator.preferred_users_lists.find(params[:list_id])
-      list.update(users: [users, list.users].flatten.uniq)
-      redirect_to [:admin, list]
-    else
+    if params.key?("add_to_list")
+      lists = current_administrator.preferred_users_lists.where(id: params[:list_ids])
+      lists.each do |list|
+        list.update(users: [users, list.users].flatten.uniq)
+      end
+
+      redirect_to [:admin, lists.first]
+    elsif params.key?("export")
       file = Exporter::Users.new(users, current_administrator).generate
       send_data file.read, filename: "#{Time.zone.today}_e-recrutement_vivers.xlsx"
     end

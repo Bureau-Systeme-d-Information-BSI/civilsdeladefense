2aa7669fa2f9f6b3aa36a4dc80eed467342ee50b
Jean Bertrand <jean@scalingo.com>
2021-12-09 11:36:54 +0100

    feat: script to transfer from OSU to OSS

---
+++ anssi-staging-transfer.sh
@@ -0,0 +1,13 @@
+set -x -e
+
+INSTANCE="erecrutement-anssi-staging"
+OLD_BUCKET="talentssi-staging"
+
+# Copy old OSU data to new OSS
+rclone copyto -P osu-$INSTANCE:$OLD_BUCKET oos-$INSTANCE:$INSTANCE --checkers 16 --transfers 16
+
+# Fix ACL on new OSS
+FOLDERS="operator_logos partner_1_logos partner_2_logos partner_3_logos logo_horizontals testimony_logos image_backgrounds"
+for FOLDER in $FOLDERS; do
+  aws s3 cp --recursive --acl public-read s3://$INSTANCE/organizations/$FOLDER s3://$INSTANCE/organizations/$FOLDER --metadata-directive REPLACE --endpoint https://oos.cloudgouv-eu-west-1.outscale.com --profile oos-$INSTANCE --page-size=100000 --cli-read-timeout 0
+done

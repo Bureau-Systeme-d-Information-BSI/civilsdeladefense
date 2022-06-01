2a87741f9490f552a7179c403cfd27520bd19ae4
Sebastien Carceles <sebastien.carceles@gmail.com>
2022-06-01 11:23:57 +0200

    redirect to admin users after user deletion

---
+++ spec/requests/admin/users_controller_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Admin::UsersController, type: :request do
+  describe "DELETE /admins/candidats/:id" do
+    it "destroys the user and redirects to index" do
+      sign_in create(:administrator)
+
+      user = create(:confirmed_user)
+      delete admin_user_path(user)
+
+      expect { user.reload }.to raise_error ActiveRecord::RecordNotFound
+      expect(response).to redirect_to(admin_users_path)
+    end
+  end
+end

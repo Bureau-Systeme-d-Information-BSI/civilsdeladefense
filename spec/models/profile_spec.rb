67c16a573b095d8b7beff8897792517fcb5615f4
Sébastien Carceles <sebastien.carceles@beta.gouv.fr>
2022-06-07 10:21:25 +0200

    Don't create a model in a before all (#1262)
    As data is not wiped after the spec has been run, avoid creating a
    model in a before all.

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -3,18 +3,15 @@
 require "rails_helper"
 
 RSpec.describe Profile, type: :model do
-  before(:all) do
-    @job_application = create(:job_application)
-    @profile = @job_application.profile
-  end
+  let(:profile) { create(:job_application).profile }
 
   it "is valid with valid attributes" do
-    expect(@profile).to be_valid
+    expect(profile).to be_valid
   end
 
   it "has correct gender" do
-    @profile.gender = 2
-    expect(@profile.gender).to eq("male")
+    profile.gender = 2
+    expect(profile.gender).to eq("male")
   end
 end
 

5d8d4c8e30446b0be51f26fa27ed5b8ec73101eb
Sebastien Carceles <sebastien.carceles@gmail.com>
2022-06-13 15:58:56 +0200

    add Sentry performance tracking

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,4 +5,10 @@ Sentry.init do |config|
   config.enabled_environments = %w[production]
   config.excluded_exceptions += ["ActionController::RoutingError", "ActiveRecord::RecordNotFound"]
   config.send_default_pii = true
+
+  # Performance tracking:
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.traces_sampler = lambda do |context|
+    true
+  end
 end

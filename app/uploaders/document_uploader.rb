7a944112a18e871ae9ec537ed0c8e48a96ff4fa7
Sébastien Carceles <sebastien.carceles@gmail.com>
2022-06-22 15:49:57 +0200

    fix Rails/UnknownEnv

--- app/uploaders/document_uploader.rb
+++ app/uploaders/document_uploader.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class DocumentUploader < CommonUploader
-  encrypt unless Rails.env.test? || Rails.env.cucumber?
+  encrypt unless Rails.env.test? || Rails.env.cucumber? # rubocop:disable Rails/UnknownEnv
 
   def content_type_allowlist
     %r{application/pdf}

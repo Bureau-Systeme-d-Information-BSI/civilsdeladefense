78b2a73f2d6e8660f71a57d9a6a28da8919dcbe6
Sébastien Carceles <sebastien.carceles@gmail.com>
2022-06-22 14:32:03 +0200

    fix Lint/UselessAssignment

--- spec/views/admin/job_applications/show.html.haml_spec.rb
+++ spec/views/admin/job_applications/show.html.haml_spec.rb
@@ -6,9 +6,9 @@ RSpec.describe "admin/job_applications/show", type: :view do
   it "renders" do
     job_application = assign(:job_application, create(:job_application))
     job_offer = assign(:job_offer, job_application.job_offer)
-    message = assign(:message, build(:message, job_application: job_application))
-    email = assign(:email, build(:email, job_application: job_application))
-    other_job_applications = create_list(:job_application, 5, job_offer: job_offer)
+    assign(:message, build(:message, job_application: job_application))
+    assign(:email, build(:email, job_application: job_application))
+    create_list(:job_application, 5, job_offer: job_offer)
 
     render
   end

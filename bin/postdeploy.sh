7c7868e452cb01b63b97bace3be976938af46e87
Sébastien Carceles <sebastien.carceles@beta.gouv.fr>
2022-07-05 15:48:27 +0200

    setup: don't monitor staging instances with sentry (#1322)

--- bin/postdeploy.sh
+++ bin/postdeploy.sh
@@ -6,7 +6,8 @@ if [ "$DO_NOT_POSTDEPLOY" = "1" ] ; then
   exit 0
 else
   bundle exec rails db:migrate
-  if [ "$IS_REVIEW_APP" != "true" ]; then
+  if ([ "$IS_REVIEW_APP" != "true" ] && [ -v SENTRY_DEPLOY_HOOK ]); then
+    echo "Sentry deploy hook"
     curl $SENTRY_DEPLOY_HOOK -X POST -H 'Content-Type: application/json' -d "{\"version\": \"${CONTAINER_VERSION}\"}"
   fi
 fi
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,14 +1,16 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  config.dsn = ENV["SENTRY_DSN"]
-  config.enabled_environments = %w[production]
-  config.excluded_exceptions += ["ActionController::RoutingError", "ActiveRecord::RecordNotFound"]
-  config.send_default_pii = true
+if ENV["SENTRY_DSN"].present?
+  Sentry.init do |config|
+    config.dsn = ENV["SENTRY_DSN"]
+    config.enabled_environments = %w[production]
+    config.excluded_exceptions += ["ActionController::RoutingError", "ActiveRecord::RecordNotFound"]
+    config.send_default_pii = true
 
-  # Performance tracking:
-  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-  config.traces_sampler = lambda do |context|
-    true
+    # Performance tracking:
+    config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+    config.traces_sampler = lambda do |context|
+      true
+    end
   end
 end

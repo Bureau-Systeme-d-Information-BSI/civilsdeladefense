0b0494a2c35d60bc4e4445f1756dfd53e1f03ad2
sebastiencarceles <sebastien.carceles@gmail.com>
2022-07-19 15:06:15 +0200

    to fixup

--- spec/requests/account/job_applications_spec.rb
+++ spec/requests/account/job_applications_spec.rb
@@ -19,6 +19,20 @@ RSpec.describe "Account::JobApplications", type: :request do
     end
   end
 
+  describe "GET /espace-candidat/mes-candidatures" do
+    let(:job_application) { create(:job_application, user: user) }
+    subject(:show_request) { get account_job_application_path(job_application) }
+
+    it "is successful" do
+      show_request
+      expect(response).to be_successful
+    end
+
+    it "renders the template" do
+      expect(show_request).to render_template(:show)
+    end
+  end
+
   describe "GET /espace-candidat/mes-candidatures/:id/offre" do
     let(:job_application) { create(:job_application, user: user) }
     subject(:job_offer_request) { get job_offer_account_job_application_path(job_application) }
@@ -30,6 +44,20 @@ RSpec.describe "Account::JobApplications", type: :request do
 
     it "renders the template" do
       expect(job_offer_request).to render_template(:job_offer)
+    end
+  end
+
+  describe "GET /espace-candidat/mes-candidatures/:id/offre" do
+    let(:job_application) { create(:job_application, user: user) }
+    subject(:profile_request) { get profile_account_job_application_path(job_application) }
+
+    it "is successful" do
+      profile_request
+      expect(response).to be_successful
+    end
+
+    it "renders the template" do
+      expect(profile_request).to render_template(:profile)
     end
   end
 end

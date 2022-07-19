50d9bc410818d390bdf0c07b00058a89629e13be
sebastiencarceles <sebastien.carceles@gmail.com>
2022-07-19 13:35:08 +0200

    update - to fixup

--- spec/requests/account/users_spec.rb
+++ spec/requests/account/users_spec.rb
@@ -20,6 +20,27 @@ RSpec.describe "Account::Users", type: :request do
     end
   end
 
+  describe "PATCH /espace-candidat/mon-compte" do
+    let(:new_first_name) { "Sebastien" }
+    subject(:update_request) { patch account_user_path, params: {user: {first_name: new_first_name}} }
+
+    it "redirects to edit_account_user_path" do
+      update_request
+      expect(response).to redirect_to(edit_account_user_path)
+    end
+
+    it "updates the user" do
+      expect { update_request }.to change { user.reload.first_name }.to(new_first_name)
+    end
+
+    it "shows an error when the user can't be updated" do
+      allow_any_instance_of(User).to receive(:update).and_return(false)
+
+      update_request
+      expect(response).to render_template(:edit)
+    end
+  end
+
   describe "DELETE /espace-candidat/mon-compte" do
     subject(:destroy_request) {
       delete account_user_path, params: {

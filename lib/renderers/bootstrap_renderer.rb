e291f427be30814ee6b4364b351de629f2441fd6
sebastiencarceles <sebastien.carceles@gmail.com>
2022-07-19 08:37:27 +0200

    crash fix

--- lib/renderers/bootstrap_renderer.rb
+++ lib/renderers/bootstrap_renderer.rb
@@ -12,7 +12,7 @@ class BootstrapRenderer < WillPaginate::ActionView::LinkRenderer
     @options[:class] ||= ""
     @options[:class] += " justify-content-center"
 
-    tag.nav(tag("ul", html, class: @options[:class]))
+    tag("nav", tag("ul", html, class: @options[:class])) # rubocop:disable Rails/ContentTag
   end
 
   protected

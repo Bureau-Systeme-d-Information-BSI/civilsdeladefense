6fc31ea1371945c48aba3db2a964b2f3cdee6715
sebastiencarceles <sebastien.carceles@gmail.com>
2022-07-19 11:14:53 +0200

    destroy: to fixup

--- spec/requests/account/users_spec.rb
+++ spec/requests/account/users_spec.rb
@@ -19,4 +19,30 @@ RSpec.describe "Account::Users", type: :request do
       expect(response).to render_template(:show)
     end
   end
+
+  describe "DELETE /espace-candidat/mon-compte" do
+    subject(:destroy_request) {
+      delete account_user_path, params: {
+        user: {
+          current_password: attributes_for(:confirmed_user)[:password]
+        }
+      }
+    }
+
+    it "redirects to the root path" do
+      destroy_request
+      expect(response).to redirect_to(root_path)
+    end
+
+    it "destroys the user" do
+      expect { destroy_request }.to change { User.count }.by(-1)
+    end
+
+    it "shows an error when the user can't be destroyed" do
+      allow_any_instance_of(User).to receive(:destroy_with_password).and_return(false)
+
+      destroy_request
+      expect(response).to render_template(:want_destroy)
+    end
+  end
 end

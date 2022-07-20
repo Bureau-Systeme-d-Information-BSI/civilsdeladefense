28f4d1fb1fd9f91317b84b52026214ed949d410c
sebastiencarceles <sebastien.carceles@gmail.com>
2022-07-20 11:31:39 +0200

    spec: cover admin::job_application_file
    see #1319

---
+++ spec/requests/admin/job_application_files_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Admin::JobApplicationFiles", type: :request do
+  let(:job_application_file) { create(:job_application_file) }
+  let(:job_application) { job_application_file.job_application }
+
+  before { sign_in create(:administrator) }
+
+  describe "GET /admin/candidatures/:job_application_id/job_application_files/:id" do
+    subject(:show_request) {
+      get admin_job_application_job_application_file_path(job_application, job_application_file)
+    }
+
+    it "sends the job application file content" do
+      show_request
+      expect(response.headers["Content-Type"]).to eq("application/pdf")
+    end
+
+    it "renders an error when the job application file content is not found" do
+      job_application_file
+      allow_any_instance_of(JobApplicationFile).to receive(:content).and_return(nil)
+
+      show_request
+      expect(response.status).to have_http_status(:not_found)
+    end
+  end
+end

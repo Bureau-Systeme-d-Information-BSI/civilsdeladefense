264ce194f269902b8609893bce71f5f5f0ed18ca
sebastiencarceles <sebastien.carceles@gmail.com>
2022-07-21 12:04:30 +0200

    foreign language levels

---
+++ spec/requests/admin/settings/foreign_language_levels_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Admin::Settings::ForeignLanguageLevels", type: :request do
+  it_behaves_like "an admin setting", :foreign_language_level, :name, "a new name"
+end

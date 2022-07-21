2d275375f13e419c38d56556e6541d29c539d388
Sébastien Carceles <sebastien.carceles@beta.gouv.fr>
2022-07-21 14:47:25 +0200

    spec: cover admin preferred users (#1340)
    see #1319

--- spec/requests/admin/preferred_users_spec.rb
+++ spec/requests/admin/preferred_users_spec.rb
@@ -10,10 +10,20 @@ RSpec.describe "Admin::PreferredUsers", type: :request do
   before { sign_in preferred_users_list.administrator }
 
   describe "GET /destroy" do
+    subject(:destroy_request) { delete admin_preferred_user_path(preferred_user) }
+
     it "removes user from the list and redirects to the list" do
-      delete admin_preferred_user_path(preferred_user)
+      destroy_request
       expect(response).to redirect_to(admin_preferred_users_list_path(preferred_users_list))
       expect(preferred_users_list.users).not_to include(user)
+    end
+
+    it "redirects if the preferred user can't be destroyed" do
+      allow_any_instance_of(PreferredUser).to receive(:destroy).and_return(false)
+
+      destroy_request
+      expect(response).to redirect_to(admin_preferred_users_list_path(preferred_users_list))
+      expect(preferred_users_list.users).to include(user)
     end
   end
 end

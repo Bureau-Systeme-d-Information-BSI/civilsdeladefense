f99b2a61dd128dc57546f7587f145658cbcdb065
sebastiencarceles <sebastien.carceles@gmail.com>
2022-07-21 12:07:35 +0200

    frequently asked questions

---
+++ spec/requests/admin/settings/frequently_asked_questions_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Admin::Settings::FrequentlyAskedQuestions", type: :request do
+  it_behaves_like "an admin setting", :frequently_asked_question, :name, "a new name"
+  it_behaves_like "a movable admin setting", :frequently_asked_question
+end

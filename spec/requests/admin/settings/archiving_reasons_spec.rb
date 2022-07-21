d84f2a7219f575c6cfd3aab2c93d7843525d9fb9
sebastiencarceles <sebastien.carceles@gmail.com>
2022-07-21 14:23:45 +0200

    archiving reason

--- spec/requests/admin/settings/archiving_reasons_spec.rb
+++ spec/requests/admin/settings/archiving_reasons_spec.rb
@@ -2,14 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe Admin::Settings::ArchivingReasonsController, type: :request do
-  describe "GET /admin/parametres/archiving_reasons" do
-    it "lists the archiving reasons" do
-      archiving_reason = create(:archiving_reason)
-      sign_in create(:administrator)
-
-      get admin_settings_archiving_reasons_path
-      expect(response.body).to include(archiving_reason.name)
-    end
-  end
+RSpec.describe "Admin::Settings::ArchivingReasons", type: :request do
+  it_behaves_like "an admin setting", :archiving_reason, :name, "a new name"
+  it_behaves_like "a movable admin setting", :archiving_reason
 end

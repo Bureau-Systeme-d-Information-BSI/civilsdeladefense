8730b62afc1924a82cbb0f3a2a7373bcb80ccbe3
Sébastien Carceles <sebastien.carceles@beta.gouv.fr>
2022-07-22 15:16:03 +0200

    spec: cover admin::stats (#1346)
    * spec: cover admin::stats
    
    see #1319

--- app/controllers/admin/stats/recruitments_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Admin::Stats::RecruitmentsController < Admin::Stats::BaseController
-  # GET /admin/stats/recrutements
-  # GET /admin/stats/recrutements.json
-  def index
-  end
-end
---
+++ spec/requests/admin/stats/job_offers_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Admin::Stats::JobOffers", type: :request do
+  before { sign_in create(:administrator) }
+
+  describe "GET /admin/stats/job_offers" do
+    context "when the format is html" do
+      subject(:index_request) { get admin_stats_job_offers_path, params: {s: "foo"} }
+
+      it "renders the template" do
+        expect(index_request).to render_template(:index)
+      end
+    end
+
+    context "when the format is xlsx" do
+      subject(:index_request) { get admin_stats_job_offers_path(format: :xlsx), params: {s: "foo"} }
+
+      it "exports the stats as a xlsx file" do
+        index_request
+        expect(response.headers["Content-Type"]).to eq(
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+      end
+    end
+  end
+end

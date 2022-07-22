360e3f997d4842e9afd1e95e0b26d9317b64f210
sebastiencarceles <sebastien.carceles@gmail.com>
2022-07-22 14:18:06 +0200

    spec: cover admin::salary ranges

---
+++ spec/requests/admin/salary_ranges_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "Admin::SalaryRanges", type: :request do
+  before { sign_in create(:administrator) }
+
+  describe "GET /admin/salary_ranges" do
+    context "when the factors are provided" do
+      it "returns the salary ranges" do
+        salary_range = create(:salary_range)
+
+        get admin_salary_ranges_path, params: {
+          job_offer: {
+            sector_id: salary_range.sector_id,
+            professional_category_id: salary_range.professional_category_id,
+            experience_level_id: salary_range.experience_level_id
+          }
+        }
+        expect(response.body).to eq([salary_range].to_json)
+      end
+    end
+
+    context "when no factor is provided" do
+      it "returns an empty list" do
+        get admin_salary_ranges_path
+        expect(response.body).to eq([].to_json)
+      end
+    end
+  end
+end

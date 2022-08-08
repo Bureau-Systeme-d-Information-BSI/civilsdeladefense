2cc027b2d35fcae66b2bc9cdfb88d3da7d78ea44
sebastiencarceles <sebastien.carceles@gmail.com>
2022-08-08 11:01:42 +0200

    :lock: secure devise session cookie
    see #1367

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -161,7 +161,10 @@ Devise.setup do |config|
 
   # Options to be passed to the created cookie. For instance, you can set
   # secure: true in order to force SSL only cookies.
-  # config.rememberable_options = {}
+  config.rememberable_options = {
+    secure: true,
+    http_only: true
+  }
 
   # ==> Configuration for :validatable
   # Range for password length.

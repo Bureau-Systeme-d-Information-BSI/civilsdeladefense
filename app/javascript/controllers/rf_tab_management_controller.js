67b2888a9a17721337c9d05618467a5703726175
sebastiencarceles <sebastien.carceles@gmail.com>
2022-08-24 11:02:41 +0200

    :children_crossing: manage attachments: job application (user)

--- app/javascript/controllers/rf_tab_management_controller.js
+++ app/javascript/controllers/rf_tab_management_controller.js
@@ -4,6 +4,10 @@ export default class extends Controller {
   static targets = [ 'tablist', 'panel' ]
 
   connect() {
+    this.resize()
+  }
+  
+  resize() {
     var height = this.panelTarget.offsetHeight + this.tablistTarget.offsetHeight
     this.element.style.height = height + 'px'
   }

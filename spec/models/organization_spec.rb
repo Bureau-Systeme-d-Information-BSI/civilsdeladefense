634c6a9f15142b01203e8b91d6edf03db9762730
Sébastien Carceles <sebastien.carceles@beta.gouv.fr>
2022-08-26 16:34:07 +0200

    :fire: remove useless trackers (#1393)
    closes #1367

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -77,6 +77,4 @@ end
 #  youtube_url                   :string
 #  created_at                    :datetime         not null
 #  updated_at                    :datetime         not null
-#  atinternet_site_id            :string
-#  matomo_site_id                :string
 #

103f7e2170ffbd3f476a60b38b432b1fa8d49e4d
Sébastien Carceles <sebastien.carceles@beta.gouv.fr>
2023-04-28 10:38:45 +0200

    🗃️ Drop admin_emails table (#1490)
    * 🗃️ Drop admin_emails table
    
    * fixup: migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_20_144305) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_28_082102) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"
@@ -44,15 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_20_144305) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "admin_emails", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "email"
-    t.string "service_name"
-    t.text "data"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_admin_emails_on_email"
   end
 
   create_table "administrators", id: :uuid, default: -> { "public.gen_random_uuid()" }, force: :cascade do |t|

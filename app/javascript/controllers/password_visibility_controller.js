465a673d624604e03f1f59eff8588bc0c3c7e12d
Sébastien Carceles <sebastien.carceles@gmail.com>
2024-01-27 11:07:04 +0100

    ✨ Show / hide password
    - add icon in password input to toggle visibility
    
    - add stimulus controller to toggle visibility
    
    - add css
    
    - add missing font awesome icon

---
+++ app/javascript/controllers/password_visibility_controller.js
@@ -0,0 +1,31 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "passwordInput", "showButton", "hideButton" ]
+  
+  toggle() {
+    if (this.isPasswordVisible()) {
+      this.hidePassword()
+    } else {
+      this.showPassword()
+    }
+  }
+  
+  // private
+  
+  isPasswordVisible() {
+    return this.passwordInputTarget.type === "text"
+  }
+
+  showPassword() {
+    this.passwordInputTarget.type = "text"
+    this.showButtonTarget.classList.add("d-none")
+    this.hideButtonTarget.classList.remove("d-none")
+  }
+
+  hidePassword() {
+    this.passwordInputTarget.type = "password"
+    this.showButtonTarget.classList.remove("d-none")
+    this.hideButtonTarget.classList.add("d-none")
+  }
+}

d2d7ec8ec0276811b9bbcb5a7e4f160d04db3b39
Sébastien Carceles <sebastien.carceles@gmail.com>
2024-03-09 10:50:16 +0100

    ⬆️ Upgrade Rails to 7.1.3.2

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,11 @@ module Civilsdeladefense
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
+
     config.time_zone = "Paris"
 
     config.i18n.available_locales = [:fr]
@@ -62,8 +67,6 @@ module Civilsdeladefense
     config.eager_load_paths << Rails.root.join("lib/services")
     config.eager_load_paths << Rails.root.join("lib/renderers")
     config.autoload_paths << Rails.root.join("lib/services")
-    config.autoload_paths << Rails.root.join("lib/renderers")
-
-    config.active_job.queue_adapter = :sidekiq
+    config.autoload_paths << Rails.root.join("extras")
   end
 end

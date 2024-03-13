3ecec39122cc0cc63e441ff84979929aadcee898
Sébastien Carceles <sebastien.carceles@gmail.com>
2024-03-13 15:34:38 +0100

    Setup Rails 7.1 default config

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module Civilsdeladefense
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 7.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -41,6 +41,9 @@ module Civilsdeladefense
       "align-right",
       "align-justify"
     ]
+
+    # Don't disable deprecated singular associations names
+    config.active_record.allow_deprecated_singular_associations_name = true
 
     if Rails.env.production?
       # Cache assets for 1 year

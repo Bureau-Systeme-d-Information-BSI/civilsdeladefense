6f64c727fe20560eeb2054bcf653e58e2690b356
Sébastien Carceles <sebastien.carceles@gmail.com>
2024-07-08 09:25:14 +0200

    :bug: Fix job offers export when sector is nil

--- lib/services/exporter/job_offer.rb
+++ lib/services/exporter/job_offer.rb
@@ -9,17 +9,15 @@ class Exporter::JobOffer < Exporter::StatJobApplications
     format_stat
     add_row
     add_row("Candidatures")
-    job_offer.job_applications.each do |ja|
-      add_row(format_job_application(ja))
-    end
+    job_offer.job_applications.each { |ja| add_row(format_job_application(ja)) }
   end
 
   def format_job_offer
     [
       job_offer.identifier,
       job_offer.title,
-      job_offer.employer.name,
-      job_offer.sector.name,
+      job_offer.employer&.name,
+      job_offer.sector&.name,
       job_offer.contract_type&.name&.presence || "",
       job_offer.contract_duration_name,
       job_offer.job_applications_count,
@@ -27,7 +25,7 @@ class Exporter::JobOffer < Exporter::StatJobApplications
       JobApplication.human_attribute_name("state/#{job_offer.most_advanced_job_applications_state}"),
       job_offer.published_at&.strftime("%d/%m/%Y %H:%M"),
       job_offer.location,
-      job_offer.category.name,
+      job_offer.category&.name,
       job_offer.estimate_monthly_salary_net,
       job_offer.estimate_annual_salary_gross,
       job_offer.is_remote_possible ? "Télétravail" : "",

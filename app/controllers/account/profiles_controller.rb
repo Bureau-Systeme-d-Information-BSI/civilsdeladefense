bcac2812b42694eb1713cbb87179db785dde8fbc
Sébastien Carceles <sebastien.carceles@gmail.com>
2024-09-04 08:29:10 +0200

    Edit languages on my profile page

--- app/controllers/account/profiles_controller.rb
+++ app/controllers/account/profiles_controller.rb
@@ -5,6 +5,7 @@ class Account::ProfilesController < Account::BaseController
   end
 
   def update
+    @profile.profile_foreign_languages = []
     @profile.assign_attributes(profile_params)
     if @profile.save
       redirect_to edit_account_profiles_path, notice: t(".success")
@@ -18,6 +19,10 @@ class Account::ProfilesController < Account::BaseController
   def find_profile = @profile = current_user.profile.presence || current_user.build_profile
 
   def profile_params
-    params.require(:profile).permit(:availability_range_id, :study_level_id)
+    params.require(:profile).permit(
+      :availability_range_id,
+      :study_level_id,
+      profile_foreign_languages_attributes: %i[foreign_language_id foreign_language_level_id]
+    )
   end
 end

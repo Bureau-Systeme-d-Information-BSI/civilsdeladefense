b0159847e929be4b9d8e41e3c01acce49315f65f
Sébastien Carceles <sebastien.carceles@gmail.com>
2024-09-10 14:38:55 +0200

    Add profile page (#1803)
    * Edit user profile
    
    Add a new page to allow user to modify it's own profile.
    
    * Edit languages on my profile page
    
    * Edit experience on my profile page

---
+++ app/controllers/account/profiles_controller.rb
@@ -0,0 +1,30 @@
+class Account::ProfilesController < Account::BaseController
+  before_action :find_profile
+
+  def edit
+  end
+
+  def update
+    @profile.profile_foreign_languages = []
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to edit_account_profiles_path, notice: t(".success")
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def find_profile = @profile = current_user.profile.presence || current_user.build_profile
+
+  def profile_params
+    params.require(:profile).permit(
+      :availability_range_id,
+      :study_level_id,
+      :experience_level_id,
+      :has_corporate_experience,
+      profile_foreign_languages_attributes: %i[foreign_language_id foreign_language_level_id]
+    )
+  end
+end

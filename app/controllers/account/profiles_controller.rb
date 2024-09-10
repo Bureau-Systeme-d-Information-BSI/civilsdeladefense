9467c7f1c839c67e521225e3ff65a432d2348000
Sébastien Carceles <sebastien.carceles@gmail.com>
2024-09-10 15:47:42 +0200

    Edit departments on My profile page

--- app/controllers/account/profiles_controller.rb
+++ app/controllers/account/profiles_controller.rb
@@ -7,6 +7,7 @@ class Account::ProfilesController < Account::BaseController
   def update
     @profile.profile_foreign_languages = []
     @profile.category_experience_levels = []
+    @profile.department_profiles = []
     @profile.assign_attributes(profile_params)
     if @profile.save(context: :profile)
       redirect_to edit_account_profiles_path, notice: t(".success")
@@ -26,7 +27,8 @@ class Account::ProfilesController < Account::BaseController
       :experience_level_id,
       :has_corporate_experience,
       profile_foreign_languages_attributes: %i[foreign_language_id foreign_language_level_id],
-      category_experience_levels_attributes: %i[category_id experience_level_id]
+      category_experience_levels_attributes: %i[category_id experience_level_id],
+      department_profiles_attributes: %i[department_id]
     )
   end
 end

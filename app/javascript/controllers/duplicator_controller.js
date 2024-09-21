bf1b89cfce10ae62691847218944fc973168ba55
Sébastien Carceles <sebastien.carceles@gmail.com>
2024-09-21 12:07:40 +0200

    Limit CategoryExperienceLevels to max 3

--- app/javascript/controllers/duplicator_controller.js
+++ app/javascript/controllers/duplicator_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "duplicated", "root", "element" ]
+  static targets = [ "duplicated", "root", "element", "addButton" ]
+  static values = { max: Number }
+
+  connect() {
+    this.setAddButtonVisibility();
+  }
 
   duplicate() {
     var cln = this.duplicatedTarget.cloneNode(true);
@@ -12,9 +17,27 @@ export default class extends Controller {
       element.name = element.name.replace('index', counter)
     })
     this.rootTarget.appendChild(cln);
+
+    this.setAddButtonVisibility();
   }
 
   remove(event) {
     event.currentTarget.parentNode.parentNode.remove();
+
+    this.setAddButtonVisibility();
+  }
+
+  // private
+
+  setAddButtonVisibility() {
+    if (typeof this.maxValue === "undefined" || this.maxValue === null || this.maxValue === 0) {
+      return;
+    }
+
+    if (this.elementTargets.length >= this.maxValue) {
+      this.addButtonTarget.classList.add("d-none");
+    } else {
+      this.addButtonTarget.classList.remove("d-none");
+    }
   }
 }

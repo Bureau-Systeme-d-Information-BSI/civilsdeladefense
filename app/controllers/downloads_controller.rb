1deee2279f426efb0933eac4d57ae42f1140468c
Sébastien Carceles <sebastien.carceles@gmail.com>
2024-12-22 12:14:14 +0100

    Add download endpoint (#1886)
    Add an endpoint to allow to download a file (for exemple a user's photo), given :
    - the resource ID
    - the resource type
    - the attribute name
    
    The endpoint requires a "X-Download-Secret-Key" header to be valid, or it returns "unauthorized".

---
+++ app/controllers/downloads_controller.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class DownloadsController < ApplicationController
+  before_action :authenticate_request
+
+  def show = send_data(attribute.read, filename: attribute.filename, type: attribute.content_type)
+
+  private
+
+  def authenticate_request
+    head :unauthorized unless request.headers["X-Download-Secret-Key"] == ENV["DOWNLOAD_SECRET_KEY"]
+  end
+
+  def resource = @resource ||= sanitize_resource_type.find(params[:id])
+
+  def attribute = @attribute ||= sanitize_attribute
+
+  def sanitize_resource_type
+    case params[:resource_type]
+    when "User"
+      User
+    else
+      throw "Invalid resource type"
+    end
+  end
+
+  def sanitize_attribute
+    case params[:attribute_name]
+    when "photo"
+      resource.photo
+    else
+      throw "Invalid attribute"
+    end
+  end
+end

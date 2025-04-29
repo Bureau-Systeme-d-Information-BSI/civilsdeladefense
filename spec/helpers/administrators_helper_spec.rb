aa0e695d4ebc3f8fa12a5121533012bfbf299996
Sébastien Carceles <sebastien.carceles@gmail.com>
2025-04-29 15:35:40 +0200

    Update navbar admin roles and employers

--- spec/helpers/administrators_helper_spec.rb
+++ spec/helpers/administrators_helper_spec.rb
@@ -16,4 +16,23 @@ RSpec.describe AdministratorsHelper do
       it { is_expected.to be_blank }
     end
   end
+
+  describe "#employers" do
+    subject { helper.employers(administrator) }
+
+    context "when administrator is a functional administrator" do
+      let(:administrator) { build(:administrator, roles: [:functional_administrator]) }
+
+      it { is_expected.to be_blank }
+    end
+
+    context "when administrator is not a functional administrator" do
+      let(:administrator) { build(:administrator, roles: [:employer_recruiter]) }
+      let(:employers) { [build(:employer, code: "EMP1"), build(:employer, code: "EMP2")] }
+
+      before { allow(administrator).to receive(:employers).and_return(employers) }
+
+      it { is_expected.to eq(employers.pluck(:code).to_sentence) }
+    end
+  end
 end

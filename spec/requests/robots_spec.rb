a6241859ba3415cb016a8aba3dc167140536a4a7
Sébastien Carceles <sebastien.carceles@gmail.com>
2025-10-26 09:25:30 +0100

    Update robots.txt

--- spec/requests/robots_spec.rb
+++ spec/requests/robots_spec.rb
@@ -4,8 +4,8 @@ require "rails_helper"
 
 RSpec.describe "Robots" do
   describe "GET /robots" do
-    it "renders the template" do
-      expect(get(robots_path(format: :txt))).to render_template(:show)
-    end
+    subject { get robots_path(format: :txt) }
+
+    it { is_expected.to render_template(:show) }
   end
 end

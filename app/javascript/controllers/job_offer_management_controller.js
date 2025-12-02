60f1d0383852cc2fc4da8d905b3e6452e4ae2433
Sébastien Carceles <sebastien.carceles@gmail.com>
2025-12-02 08:22:12 +0100

    JobOffer edit: show error msg when admin not found (#2032)

--- app/javascript/controllers/job_offer_management_controller.js
+++ app/javascript/controllers/job_offer_management_controller.js
@@ -1,61 +1,72 @@
-import Rails from '@rails/ujs'
-import Url from 'domurl'
-import { Controller } from "@hotwired/stimulus"
+import Rails from "@rails/ujs";
+import Url from "domurl";
+import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
   removeRecord(event) {
-    let node = event.currentTarget
+    let node = event.currentTarget;
 
-    let job_offer_actor_node = node.closest('.job-offer-actor')
-    let hidden_field = job_offer_actor_node.querySelector('[name$="[_destroy]"]')
+    let job_offer_actor_node = node.closest(".job-offer-actor");
+    let hidden_field = job_offer_actor_node.querySelector(
+      '[name$="[_destroy]"]',
+    );
 
-    let hidden_value = hidden_field.value
-    if (hidden_value === 'true') {
-      hidden_field.value = 'false'
-      job_offer_actor_node.classList.remove('opacify')
+    let hidden_value = hidden_field.value;
+    if (hidden_value === "true") {
+      hidden_field.value = "false";
+      job_offer_actor_node.classList.remove("opacify");
     } else {
-      hidden_field.value = 'true'
-      job_offer_actor_node.classList.add('opacify')
+      hidden_field.value = "true";
+      job_offer_actor_node.classList.add("opacify");
     }
-    event.preventDefault()
+    event.preventDefault();
   }
 
   addFields(event) {
-    let node = event.currentTarget
-    let emailField = node.previousElementSibling.querySelector('input[name=email]')
+    document.querySelector("#admin-not-found")?.classList?.add("d-none");
+
+    let node = event.currentTarget;
+    let emailField =
+      node.previousElementSibling.querySelector("input[name=email]");
     if (emailField == null) {
-      emailField = node.previousElementSibling.querySelector('select')
+      emailField = node.previousElementSibling.querySelector("select");
     }
-    let email = emailField.value
-    let url = node.getAttribute('data-url')
-    let u = new Url(url)
-    u.query.email = email
-    url = u.toString()
+    let email = emailField.value;
+    let url = node.getAttribute("data-url");
+    let u = new Url(url);
+    u.query.email = email;
+    url = u.toString();
     Rails.ajax({
-      type: 'GET',
+      type: "GET",
       url: url,
-      beforeSend: function() {
-        let formGroup = emailField.closest('.form-group')
-        formGroup.classList.remove('form-group-invalid')
-        ;[].forEach.call(formGroup.querySelectorAll('.invalid-feedback'), function(el) {
-          formGroup.removeChild(el)
-        })
-        emailField.classList.remove('is-invalid')
-        return true
+      beforeSend: function () {
+        let formGroup = emailField.closest(".form-group");
+        formGroup.classList.remove("form-group-invalid");
+        [].forEach.call(
+          formGroup.querySelectorAll(".invalid-feedback"),
+          function (el) {
+            formGroup.removeChild(el);
+          },
+        );
+        emailField.classList.remove("is-invalid");
+        return true;
       },
-      success: function(response){
-        let content = response.querySelector('form').innerHTML
-        let fieldList = node.closest('.form-actor').previousElementSibling
-        fieldList.insertAdjacentHTML('afterend', content)
-        emailField.value = ''
+      success: function (response) {
+        let content = response.querySelector("form").innerHTML;
+        let fieldList = node.closest(".form-actor").previousElementSibling;
+        fieldList.insertAdjacentHTML("afterend", content);
+        emailField.value = "";
       },
-      error: function(response){
-        let message = response.email.join(', ')
-        emailField.closest('.form-group').classList.add('form-group-invalid')
-        emailField.classList.add('is-invalid')
-        emailField.insertAdjacentHTML('afterend', `<div class=\'invalid-feedback\'>${message}</div>`)
-      }
-    })
-    event.preventDefault()
+      error: function (response) {
+        let message = response.email.join(", ");
+        emailField.closest(".form-group").classList.add("form-group-invalid");
+        emailField.classList.add("is-invalid");
+        emailField.insertAdjacentHTML(
+          "afterend",
+          `<div class=\'invalid-feedback\'>${message}</div>`,
+        );
+      },
+    });
+    event.preventDefault();
   }
 }

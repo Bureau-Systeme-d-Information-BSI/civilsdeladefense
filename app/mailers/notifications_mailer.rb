3ffed60404383cba1fe7da58cbd3695129d4c869
Sébastien Carceles <sebastien.carceles@gmail.com>
2026-02-02 08:19:37 +0100

    Add contract received email

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -39,6 +39,17 @@ class NotificationsMailer < ApplicationMailer
     mail to: @administrator.email, subject: t(".subject", service_name: @service_name)
   end
 
+  def contract_received
+    @administrator = params[:administrator]
+    @job_application = params[:job_application]
+    @job_offer = @job_application.job_offer
+    @service_name = @administrator.organization.service_name
+    @employer_recruiters = @job_application.job_offer_employer_recruiters
+    @hr_managers = @job_application.job_offer_hr_managers
+
+    mail to: @administrator.email, subject: t(".subject", service_name: @service_name)
+  end
+
   def daily_summary(administrator, data, service_name)
     @data = data
     subject = t(".subject", service_name: service_name)

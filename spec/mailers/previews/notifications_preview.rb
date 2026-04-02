8d98f6cebdce4e2e5f420fdc05c4352c298dd94d
Sébastien Carceles <sebastien.carceles@gmail.com>
2026-04-02 15:40:52 +0200

    Notify employer recruiter when email is sent by user (#2043)
    * Add scope to get employer_recruiters
    
    * Add association to get employer recruiters of a job offer
    
    * Add delegate to get job app employer recruiters
    
    * Send email notification to employer recruiters
    
    When job app state is:
    - phone_meeting
    - to_be_met
    
    We send an email notification to employer recruiters
    
    * Add missing spec
    
    * Fix link in email
    
    * Update link

--- spec/mailers/previews/notifications_preview.rb
+++ spec/mailers/previews/notifications_preview.rb
@@ -2,8 +2,10 @@
 
 # Preview all emails at http://localhost:3000/rails/mailers/notifications
 class NotificationsPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/notifications/daily_summary
-  def daily_summary
-    NotificationsMailer.daily_summary
+  def new_email
+    job_application = JobApplication.find_by(state: :phone_meeting) || JobApplication.first
+    NotificationsMailer.with(administrator: Administrator.first, job_application:).new_email
   end
+
+  def daily_summary = NotificationsMailer.daily_summary
 end

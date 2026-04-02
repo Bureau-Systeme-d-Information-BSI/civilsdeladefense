a353715d9a159feeca9ae71152500c6853b2897c
Sébastien Carceles <sebastien.carceles@gmail.com>
2026-04-02 16:26:55 +0200

    Notify managers when job app state becomes affected (#2047)
    * Notify hr manager when job app state becomes contract feedback waiting
    
    * Add contract received email
    
    * Notify managers when state becomes affected
    
    * Refactor: notifiable concern
    
    * Update wording and link
    
    * Fix post merge

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -50,10 +50,32 @@ class NotificationsMailer < ApplicationMailer
     mail to: @administrator.email, subject: t(".subject", service_name: @service_name)
   end
 
+  def affected
+    @administrator = params[:administrator]
+    @job_application = params[:job_application]
+    @job_offer = @job_application.job_offer
+    @service_name = @administrator.organization.service_name
+    @employer_recruiters = @job_application.job_offer_employer_recruiters
+    @hr_managers = @job_application.job_offer_hr_managers
+    @roles = manager_roles(@administrator)
+
+    mail to: @administrator.email, subject: t(".subject", service_name: @service_name)
+  end
+
   def daily_summary(administrator, data, service_name)
     @data = data
     subject = t(".subject", service_name: service_name)
 
     mail to: administrator.email, subject: subject
+  end
+
+  private
+
+  def manager_roles(administrator)
+    administrator
+      .roles
+      .select { |role| role.in?([:hr_manager, :payroll_manager]) }
+      .map { |role| I18n.t("activerecord.attributes.administrator/roles.#{role}") }
+      .to_sentence
   end
 end

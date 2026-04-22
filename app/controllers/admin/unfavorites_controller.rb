458d781e59f669a392bf0a4877d13271c949acdf
Sébastien Carceles <sebastien.carceles@gmail.com>
2026-04-22 16:25:26 +0200

    Can / can't update_application_preselection and update states permissions

--- app/controllers/admin/unfavorites_controller.rb
+++ app/controllers/admin/unfavorites_controller.rb
@@ -2,6 +2,8 @@ class Admin::UnfavoritesController < Admin::BaseController
   skip_load_and_authorize_resource
   load_and_authorize_resource :job_application
 
+  before_action :check_preselection_permission
+
   def create
     @job_application.preselect_as_unfavorite!
     redirect_back_or_to [:admin, @job_application]
@@ -9,6 +11,14 @@ class Admin::UnfavoritesController < Admin::BaseController
 
   def destroy
     @job_application.unpreselect_as_unfavorite!
+    redirect_back_or_to [:admin, @job_application]
+  end
+
+  private
+
+  def check_preselection_permission
+    return if current_administrator.can?(:update_application_preselection, @job_application)
+
     redirect_back_or_to [:admin, @job_application]
   end
 end
--- spec/requests/admin/unfavorites_spec.rb
+++ spec/requests/admin/unfavorites_spec.rb
@@ -10,11 +10,23 @@ RSpec.describe "Admin::Unfavorites" do
 
     let(:job_application) { create(:job_application) }
 
-    before { preselect_as_unfavorite }
+    context "when administrator can update_application_preselection" do
+      before { allow_any_instance_of(Administrator).to receive(:can?).and_return(true) }
 
-    it "preselects as unfavorite" do
-      expect(job_application.reload.preselection).to eq("unfavorite")
-      expect(response).to redirect_to(admin_job_application_path(job_application))
+      it "preselects as unfavorite" do
+        preselect_as_unfavorite
+        expect(job_application.reload.preselection).to eq("unfavorite")
+        expect(response).to redirect_to(admin_job_application_path(job_application))
+      end
+    end
+
+    context "when administrator cannot update_application_preselection" do
+      before { allow_any_instance_of(Administrator).to receive(:can?).and_return(false) }
+
+      it "does not preselect and redirects back" do
+        expect { preselect_as_unfavorite }.not_to change { job_application.reload.preselection }
+        expect(response).to redirect_to(admin_job_application_path(job_application))
+      end
     end
   end
 
@@ -25,11 +37,23 @@ RSpec.describe "Admin::Unfavorites" do
 
     let(:job_application) { create(:job_application, preselection: "unfavorite") }
 
-    before { unpreselect_as_unfavorite }
+    context "when administrator can update_application_preselection" do
+      before { allow_any_instance_of(Administrator).to receive(:can?).and_return(true) }
 
-    it "unpreselects as unfavorite" do
-      expect(job_application.reload.preselection).to eq("pending")
-      expect(response).to redirect_to(admin_job_application_path(job_application))
+      it "unpreselects as unfavorite" do
+        unpreselect_as_unfavorite
+        expect(job_application.reload.preselection).to eq("pending")
+        expect(response).to redirect_to(admin_job_application_path(job_application))
+      end
+    end
+
+    context "when administrator cannot update_application_preselection" do
+      before { allow_any_instance_of(Administrator).to receive(:can?).and_return(false) }
+
+      it "does not change the preselection and redirects back" do
+        expect { unpreselect_as_unfavorite }.not_to change { job_application.reload.preselection }
+        expect(response).to redirect_to(admin_job_application_path(job_application))
+      end
     end
   end
 end

4512506571f1a373e48b8e95763c61e6620dccda
Sébastien Carceles <sebastien.carceles@gmail.com>
2026-05-08 10:21:27 +0200

    Update schema.db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_07_140000) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_07_140000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "fuzzystrmatch"
   enable_extension "pg_trgm"

268e654ab6f6580f4d40185a8e7017214eae2347
Sébastien Carceles <sebastien.carceles@gmail.com>
2026-05-28 08:03:36 +0200

    Add SimpleCov for test coverage reporting

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+require "simplecov"
+SimpleCov.start
+
 require "spec_helper"
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)

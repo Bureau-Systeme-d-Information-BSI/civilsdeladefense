8458483447344ba99cd2b4aaa54eba93c9a01579
Mahmood Sharifi <mahmood@msharifi.dev>
2026-05-29 16:30:26 +0200

    reload only persisted job_application_files

--- app/models/job_application.rb
+++ app/models/job_application.rb
@@ -249,7 +249,12 @@ class JobApplication < ApplicationRecord
 
   def compute_files_count
     ary_start = [0, 0]
-    ary = job_application_files.reload.includes(:job_application_file_type).each_with_object(ary_start) { |job_application_file, memo|
+    files = if persisted?
+               job_application_files.reload.includes(:job_application_file_type)
+             else
+               job_application_files
+             end
+    ary = files.each_with_object(ary_start) { |job_application_file, memo|
       memo[0] += 1
       memo[1] += 1 if job_application_file.waiting_validation? &&
         job_application_file.job_application_file_type&.notification &&

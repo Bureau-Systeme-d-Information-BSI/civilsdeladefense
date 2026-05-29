2e8d9b598d56b75f470e9f3adf1d28de82fa334c
Mahmood Sharifi <59996027+msharififr@users.noreply.github.com>
2026-05-29 11:33:44 +0200

    job offer actors list updated in job offer form (#2156)
    * job offer actors list updated in job offer form
    
    * typo correction in fr.yml
    
    * make payroll_manager appear before scm

--- app/models/job_offer_actor.rb
+++ app/models/job_offer_actor.rb
@@ -25,12 +25,14 @@ class JobOfferActor < ApplicationRecord
 
   #####################################
   # Enums
+  # NOTE: role is no more used to define access rights
   enum :role, {
     employer: 0,
     grand_employer: 10,
     supervisor_employer: 20,
     brh: 30,
-    cmg: 40
+    payroll_manager: 50,
+    cmg: 40 # cmg should appear at the end of actors form, but the value is still 40
   }
 
   private

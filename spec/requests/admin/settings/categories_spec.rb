b55d91cfac8aa5f08d9e74403619df8d7cb1d36f
Sébastien Carceles <sebastien.carceles@gmail.com>
2026-06-15 08:03:33 +0200

    Cover Admin::Settings inherited resources moves and destroy via categories spec

--- spec/requests/admin/settings/categories_spec.rb
+++ spec/requests/admin/settings/categories_spec.rb
@@ -25,5 +25,56 @@ RSpec.describe "Admin::Settings::Categories" do
 
       it { expect { destroy_request }.to change(Category, :count).by(-1) }
     end
+
+    context "when the category cannot be destroyed" do
+      before do
+        allow_any_instance_of(Category).to receive(:children).and_return([])
+        allow_any_instance_of(Category).to receive(:destroy!).and_raise(ActiveRecord::RecordNotDestroyed)
+      end
+
+      it { expect(destroy_request).to redirect_to(admin_settings_categories_path) }
+
+      it { expect { destroy_request }.not_to change(Category, :count) }
+    end
+  end
+
+  describe "POST /admin/parametres/categories/:id/move_left" do
+    subject(:move_left_request) { post move_left_admin_settings_category_path(second) }
+
+    before do
+      sign_in create(:administrator)
+      first
+      second
+    end
+
+    let(:first) { create(:category, name: "First") }
+    let(:second) { create(:category, name: "Second") }
+
+    it { expect(move_left_request).to redirect_to(admin_settings_categories_path) }
+
+    it { expect { move_left_request }.to change { category_names }.to(%w[Second First]) }
+  end
+
+  describe "POST /admin/parametres/categories/:id/move_right" do
+    subject(:move_right_request) { post move_right_admin_settings_category_path(first) }
+
+    before do
+      sign_in create(:administrator)
+      first
+      second
+    end
+
+    let(:first) { create(:category, name: "First") }
+    let(:second) { create(:category, name: "Second") }
+
+    it { expect(move_right_request).to redirect_to(admin_settings_categories_path) }
+
+    it { expect { move_right_request }.to change { category_names }.to(%w[Second First]) }
+  end
+
+  private
+
+  def category_names
+    Category.order(:lft).pluck(:name)
   end
 end

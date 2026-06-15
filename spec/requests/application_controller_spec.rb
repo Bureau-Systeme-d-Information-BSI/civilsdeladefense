c2e0c93420c7457f11f9e61c1c750f0052573ecc
Sébastien Carceles <sebastien.carceles@gmail.com>
2026-06-15 08:03:10 +0200

    Cover ApplicationController basic auth and layout with specs

---
+++ spec/requests/application_controller_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "ApplicationController" do
+  describe "#basic_auth" do
+    before do
+      allow(ENV).to receive(:[]).and_call_original
+      allow(ENV).to receive(:[]).with("BASIC_AUTH").and_return("user:secret")
+      perform
+    end
+
+    context "with valid credentials" do
+      subject(:perform) do
+        get altcha_path, headers: {
+          "Authorization" => ActionController::HttpAuthentication::Basic.encode_credentials("user", "secret")
+        }
+      end
+
+      it { expect(response).to have_http_status(:ok) }
+    end
+
+    context "without credentials" do
+      subject(:perform) { get altcha_path }
+
+      it { expect(response).to have_http_status(:unauthorized) }
+    end
+  end
+
+  describe "#layout_by_resource" do
+    context "with a non-administrator devise controller" do
+      subject(:perform) { get new_user_session_path }
+
+      before { perform }
+
+      it { expect(response).to have_http_status(:ok) }
+    end
+  end
+end

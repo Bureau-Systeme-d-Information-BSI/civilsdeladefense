2fc9f09eb8f4fb950ddd5a30131d292070c96674
Mahmood Sharifi <mahmood@msharifi.dev>
2026-07-07 10:46:20 +0200

    rubocop corrections

--- config/initializers/permissions_policy.rb
+++ config/initializers/permissions_policy.rb
@@ -4,26 +4,26 @@
 # information see: https://developers.google.com/web/updates/2018/06/feature-policy
 
 Rails.application.config.permissions_policy do |policy|
-  policy.accelerometer        :none
+  policy.accelerometer :none
   policy.ambient_light_sensor :none
-  policy.autoplay             :none
-  policy.camera               :none
-  policy.display_capture      :none
-  policy.encrypted_media      :none
-  policy.fullscreen           :none
-  policy.geolocation          :none
-  policy.gyroscope            :none
-  policy.hid                  :none
-  policy.idle_detection       :none
-  policy.keyboard_map         :none
-  policy.magnetometer         :none
-  policy.microphone           :none
-  policy.midi                 :none
-  policy.payment              :none
-  policy.picture_in_picture   :none
-  policy.screen_wake_lock     :none
-  policy.serial               :none
-  policy.sync_xhr             :none
-  policy.usb                  :none
-  policy.web_share            :none
+  policy.autoplay :none
+  policy.camera :none
+  policy.display_capture :none
+  policy.encrypted_media :none
+  policy.fullscreen :none
+  policy.geolocation :none
+  policy.gyroscope :none
+  policy.hid :none
+  policy.idle_detection :none
+  policy.keyboard_map :none
+  policy.magnetometer :none
+  policy.microphone :none
+  policy.midi :none
+  policy.payment :none
+  policy.picture_in_picture :none
+  policy.screen_wake_lock :none
+  policy.serial :none
+  policy.sync_xhr :none
+  policy.usb :none
+  policy.web_share :none
 end
